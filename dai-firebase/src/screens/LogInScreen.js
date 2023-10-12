4035fc8deea387de460e48aa4e1f8fdff5044e8a
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-10-12 11:12:04 -0300

    aloja

--- dai-firebase/src/screens/LogInScreen.js
+++ dai-firebase/src/screens/LogInScreen.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, Alert } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Alert, Image, StatusBar, Pressable, Button, TextInput } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 import { initializeApp } from 'firebase/app';
 import { firebaseConfig } from '../../firebase-config';
 import { useNavigation } from '@react-navigation/native';
 import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import tailwind from "twrnc";
+
 
 const LogInScreen = () => {
   const [email, setEmail] = useState('');
@@ -55,127 +57,63 @@ const LogInScreen = () => {
       };
 
   return (
-    <View style={styles.view}>
-      <View style={styles.container}>
-        <Text style={styles.baseText}>Ingresa tu cuenta</Text>
+    <View style={tailwind`flex-1 w-full items-center justify-center bg-gray-500`}>
+    <View style={tailwind`px-4 w-full max-w-sm`}>
+      <Text style={tailwind`text-5xl font-bold mb-6 text-gray-50`}>
+        Login
+      </Text>
 
-        <View style={[styles.inputContainer, styles.button]}>
-          <TextInput
-            style={styles.input}
-            placeholder="Mail:"
-            value={email}
-            onChangeText={(text) => setEmail(text)}
-          />
-        </View>
-
-        <View style={[styles.inputContainer, styles.button]}>
-          <TextInput
-            style={styles.input}
-            placeholder="Contraseña:"
-            value={contraseña}
-            onChangeText={(text) => setContraseña(text)}
-            
-          />
-          <TouchableOpacity style={styles.showPasswordButton}>
-            
-          </TouchableOpacity>
-        </View>
-
-        <TouchableOpacity onPress={handleCreateAccount} style={styles.buttonChico}  >
-          <Text  style={styles.buttonText}>Crear Cuenta</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity onPress={handleSingIn} style={styles.buttonChico}  >
-          <Text style={styles.buttonText}>Iniciar sesión</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity onPress={handleGoogleSignIn} style={styles.buttonChico}>
-        <Text style={styles.buttonText}>Sign In with Google</Text>
-      </TouchableOpacity>
-
+      <View style={tailwind`flex flex-col gap-4 `}>
+        <TextInput
+          onChangeText={(email) => setEmail(email)}
+          
+          placeholder="Enter email address"
+          style={tailwind`text-white border border-gray-700 p-2 rounded-lg box-shadow-6`}
+        />
+        <TextInput
+          value={contraseña}
+          onChangeText={(text) => setContraseña(text)}
+          secureTextEntry={true}
+          style={tailwind`text-white border border-gray-700 p-2 rounded-lg box-shadow-6`}
+          placeholder="Enter password"
+        />
       </View>
+
+      <View style={tailwind`flex flex-row justify-between my-8`}>
+        <View style={tailwind`flex-row items-center`}>
+          <Pressable style={tailwind`bg-gray-50 h-6 w-6 rounded-sm mr-2`}></Pressable>
+          <Text style={tailwind`text-gray-50`}>Remember me</Text>
+        </View>
+        <Pressable>
+          <Text style={tailwind`text-gray-50 font-bold`}>Reset password</Text>
+        </Pressable>
+      </View>
+
+      <Pressable style={tailwind`bg-green-700 hover:bg-green-800 text-green-50 rounded-lg`} onPress={handleSingIn}>
+        <Text style={tailwind`h-10 px-4 flex-row items-center justify-center rounded-md text-center mt-5 text-white `}>Sign In</Text>
+      </Pressable>
+
+      <Pressable onPress={handleCreateAccount} style={tailwind`flex-row items-center justify-center px-4 py-2 rounded-md -mb-2`}>
+      <Icon name="" size={24} color="white" />
+      <Text style={tailwind`text-gray-200 ` } >Registrarse</Text>
+    </Pressable>
+
+      <View style={tailwind`flex flex-row justify-between my-5`}>
+        <Pressable style={tailwind`flex-row items-center bg-red-700 px-4 py-2 rounded-md`} onPress={handleGoogleSignIn}>
+          <Icon name="google" size={24} color="white" />
+          <Text style={tailwind`text-white ml-2`}>Sign in with Google</Text>
+        </Pressable>
+        <Pressable style={tailwind`flex-row items-center bg-blue-700 px-4 py-2 rounded-md`}>
+          <Icon name="facebook" size={24} color="white" />
+          <Text style={tailwind`text-white ml-2`}>Sign in with Facebook</Text>
+        </Pressable>
+      </View>
+ 
     </View>
+  </View>
   );
 };
 
-const styles = StyleSheet.create({
-  view: {
-    backgroundColor: '#EFEFEF',
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  baseText: {
-    textAlign: 'center',
-    marginTop: 180,
-    marginBottom: 30,
-    color: '#668557',
-    /*fontFamily: 'Crimson Text',*/
-    fontSize: 25,
-  /*   lineHeight: 24, */
-  },
-  container: {
-    height: '100%',
-    width: '80%',
-    alignItems:'center',
-  },
-  inputContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 10,
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 4,
-    paddingLeft: 10,
-  },
-  input: {
-    flex: 1,
-    paddingVertical: 10,
-  },
-  showPasswordButton: {
-    paddingHorizontal: 10,
-    left: 15,
-  },
-  button: {
-    alignItems: 'center',
-    borderRadius: 15,
-    backgroundColor: '#99D87D',
-    marginTop: 10,
-    paddingHorizontal: 30,
-    paddingVertical: 10,
-    shadowRadius: 15,
-    shadowColor: '#2C4521',
-    /* shadowOpacity: 0.6, */
-    elevation: 5,
-  },
-  buttonChico: {
-    justifyContent: 'center',
-    alignContent: 'center',
-    width: '50%',
-    borderRadius: 15,
-    backgroundColor: '#99D87D',
-    marginTop: 10,
-    paddingHorizontal: 30,
-    paddingVertical: 10,
-    shadowRadius: 15,
-    shadowColor: '#2C4521',
-    /* shadowOpacity: 0.6, */
-    elevation: 5,
-  },
-  buttonText: {
-    color: '#2C4521',
-    /* fontFamily: 'assets/fonts/UberMoveMedium.otf', */
-    fontSize: 16,
-    fontStyle: 'normal',
-    justifyContent:'center',
-    alignContent:'center',
-  /*   lineHeight: 18, */
-  },
-  semiCircle: {
-    /* position: 'absolute', */
-    top: 0,
-    left: 0,
-  },
-});
+
 
 export default LogInScreen;

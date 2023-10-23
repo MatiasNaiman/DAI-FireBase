4e5e623fde397e0764e6b6a78fca97c8d49e8f26
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-10-23 10:41:42 -0300

    k

--- dai-firebase/src/screens/HomeScreen/HomeScreen.js
+++ dai-firebase/src/screens/HomeScreen/HomeScreen.js
@@ -1,10 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { Text, View, Button, TextInput, ScrollView, TouchableOpacity, FlatList } from 'react-native';
+import { useState, useEffect } from 'react';
+import { Text, View, Button, TextInput, ScrollView } from 'react-native';
 import { FIREBASE_AUTH, FIRESTORE_DB } from "../../firebase/config";
-import { FIREBASE_APP } from '../../firebase/config'
 import { signOut } from 'firebase/auth';
-import { doc, getDocs, setDoc, collection, addDoc } from 'firebase/firestore';
-import { Keyboard } from 'react-native';
+import { collection, addDoc, onSnapshot, deleteDoc, doc } from 'firebase/firestore';
 
 export default function HomeScreen({ navigation, route }) {
   const userData = route.params.userData;
@@ -13,11 +11,10 @@ export default function HomeScreen({ navigation, route }) {
   const [todos, setTodos] = useState([]);
   const [todosUpdated, setTodosUpdated] = useState(0);
 
-
   const handleLogout = async () => {
     try {
       await signOut(FIREBASE_AUTH);
-      reloadUser(); //actualiza user a null se recarg y ya esta unloged
+      reloadUser(); // Actualiza user a null, se recarga y ya está unlogged.
     } catch (error) {
       console.error('Error signing out:', error);
     }
@@ -29,7 +26,7 @@ export default function HomeScreen({ navigation, route }) {
     try {
       await addDoc(collection(FIRESTORE_DB, 'todos'), {
         todo: todo,
-        id: userData.id,
+        idUser: userData.id,
         name: userData.fullName,
       });
       setTodo(''); // Limpia el campo de entrada
@@ -38,53 +35,76 @@ export default function HomeScreen({ navigation, route }) {
     }
   }
 
-  const fetchTodos = async () => {
+  const deleteMessage = async (messageId) => {
     try {
-      const querySnapshot = await getDocs(collection(FIRESTORE_DB, 'todos'));
-      const newData = querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
-      setTodos(newData);
+      const messageDocRef = doc(FIRESTORE_DB, 'todos', messageId);
+      await deleteDoc(messageDocRef);
     } catch (error) {
-      console.error('Error fetching todos: ', error);
+      console.error('Error deleting message: ', error);
     }
   }
 
-  useEffect(() => {
-    fetchTodos();
-  }, [todo]); 
+  const fetchTodos = () => {
+    const todosRef = collection(FIRESTORE_DB, 'todos');
 
+    const unsubscribe = onSnapshot(todosRef, (querySnapshot) => {
+      const newData = querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+      setTodos(newData);
+    });
+    // Devuelve una función para cancelar la suscripción cuando el componente se desmonte.
+    return unsubscribe;
+  };
+
+  useEffect(() => {
+    const unsubscribe = fetchTodos();
+    // Devuelve una función para cancelar la suscripción cuando el componente se desmonte.
+    return () => {
+      unsubscribe();
+    };
+  }, []);
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center',alignItems: 'center', backgroundColor: '#fff',}}>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff' }}>
       <Text>Home Screen</Text>
       <Text>User ID: {userData.id}</Text>
       <Text>User Email: {userData.email}</Text>
       <Text>User Full Name: {userData.fullName}</Text>
       <Button title="Logout" onPress={handleLogout} />
 
-      {/*datos*/}
-      <View style={{ flex: 1 }}>
-      <View style={{ padding: 16 }}>
-        <Text style={{ fontSize: 24, marginBottom: 16, textAlign: 'center' }}>
-          Todo-App
-        </Text>
-        <TextInput
-          style={{ borderWidth: 1, padding: 8, marginBottom: 16 }}
-          placeholder="What do you have to do today?"
-          value={todo}
-          onChangeText={(text) => setTodo(text)}
-        />
-        <Button title="Submit" onPress={addTodo} />
-      </View>
-      <ScrollView style={{ flex: 1, padding: 16 }}>
-        {todos.map((todo, index) => (
-          <Text key={todo.id} style={{ fontSize: 18, marginBottom: 8 }}>
-            {index + 1}. {todo.todo}
+      <View style={{ flex: 1, backgroundColor: "pink" }}>
+        <View style={{ padding: 16 }}>
+          <Text style={{ fontSize: 24, marginBottom: 10, textAlign: 'center' }}>
+            Mensajes
           </Text>
-        ))}
-      </ScrollView>
-    </View>
-       {/*Fin*/}
-    
+          <TextInput
+            style={{ borderWidth: 1, padding: 8, marginBottom: 10 }}
+            placeholder="Escribe un mensaje"
+            value={todo}
+            onChangeText={(text) => setTodo(text)}
+          />
+          <Button title="Submit" onPress={addTodo} />
+        </View>
+        <ScrollView style={{ flex: 1, padding: 16 }}>
+          {todos.map((message, index) => (
+            <View
+              key={message.id}
+              style={{
+                marginVertical: 8,
+                backgroundColor: message.idUser === userData.id ? '#FFDEB9' : '#C7ECFF',
+                borderRadius: 10,
+                padding: 10,
+              }}
+            >
+              <Text style={{ fontSize: 18 }}>
+                <Text style={{ color: "#AC0300", fontWeight: "bold" }}>{message.name}:</Text> {message.todo}
+              </Text>
+              {message.idUser === userData.id && (
+                <Button title="Eliminar" onPress={() => deleteMessage(message.id)} />
+              )}
+            </View>
+          ))}
+        </ScrollView>
+      </View>
     </View>
   );
 }
